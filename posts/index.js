98982f783f68a4f6ba75d0823fad17c310b46e58
RupakSapkota123 <rupaksapkota2000@gmail.com>
2022-04-20 21:27:34 +0545

    mongoose initilized

--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+// db connection
+mongoose.connect('mongodb://127.0.0.1:27017/posts', (err) => {
+if (err) {
+console.log('error >>>', err);
+} else {
+console.log('Connected to Database.....');
+}
+});
+
 
 const app = express();
 
 app.use(express.json());
 
+// server port
 app.listen(9000, () => {
 console.log('Server is running on port 9000');
 });
